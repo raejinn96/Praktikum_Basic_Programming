2148232b1c2bbfdd85202c91a8a84a64e1b11ddb
raejinn96 <smlekmew@gmail.com>
2023-07-07 11:24:18 +0700

    Sudah diupdate kodenya

--- Bilangan_Prima/main.go
+++ Bilangan_Prima/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func main(){
-	
+func main() {
+
+	var angka int
+
+	fmt.Print("Masukkan angka: ")
+	fmt.Scanln(&angka)
+
+	// syarat bilangan prima, bilangan hanya bisa dibagi dengan :
+	// 1. Angka 1
+	// 2. Bilangan itu sendiri
+	if angka <= 1 {
+		fmt.Println(angka, "Bukan bilangan prima")
+	}
+
+	akar := int(math.Sqrt(float64(angka)))
+	fmt.Printf("Akar dari bilangan %d adalah: ", angka)
+	fmt.Println(akar)
+
+	for i := 2; i <= akar; i++ {
+		if angka%i == 0 {
+			fmt.Println(angka, "Bukan merupakan bilangan prima")
+			return
+		}
+	}
+
+	fmt.Println(angka, "Adalah bilangan prima")
+	return
+
 }
