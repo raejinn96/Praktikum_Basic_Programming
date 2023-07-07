b65d758b1c5c5bf5238eb89f915ebd9285abc1b8
raejinn96 <smlekmew@gmail.com>
2023-07-07 19:26:04 +0700

    updated review

--- Luas_Trapesium/main.go
+++ Luas_Trapesium/main.go
@@ -10,9 +10,15 @@ import "fmt"
 // Tinggi (T)		= 12
 
 func main() {
-	var a, b, T uint16 = 5, 10, 12
+	var a, b, T int
+	fmt.Print("Masukkan nilai a: ")
+	fmt.Scanln(&a)
+	fmt.Print("Masukkan nilai b: ")
+	fmt.Scanln(&b)
+	fmt.Print("Masukkan nilai T: ")
+	fmt.Scanln(&T)
 
-	Luas := (a + b) * T / 2
+	Luas := float32(a + b) * float32(T) / 2
 
 	fmt.Print("Jadi luas trapesiumnya adalah: ", Luas)
 
